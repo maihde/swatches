b2156aff6ebbebebca6a99aee2c7f1fd9ef414f9
Michael Ihde <mike.ihde@gmail.com>
2020-11-08 16:27:25 -0500

    updated

--- build.py
+++ build.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 import json
 import os
-import subprocess
 import optparse
 
 parser = optparse.OptionParser()
 parser.add_option("-b", "--rebuild", action="store_true", default=False)
+parser.add_option("-e", "--executable") 
 opts, args = parser.parse_args()
 
 if not os.path.exists("output"):
     os.makedirs("output")
 
+if opts.executable== None:
+    if os.path.exists("./OpenSCAD-2019.05-x86_64.AppImage"):
+        opts.executable = "./OpenSCAD-2019.05-x86_64.AppImage"
+    elif os.path.exists("/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"):
+        opts.executable = "/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
+    else:
+        opts.executable= "openscad"
+
 with open("swatches.json") as f:
     swatches = json.load(f)
     swatch_params = swatches.get("parameterSets", {})
     for name in sorted(swatch_params):
-        if not os.path.exists(f"output/{name}.stl") or opts.rebuild:
-            print(f"Building output/{name}.stl")
-            subprocess.run([
-                "./OpenSCAD-2019.05-x86_64.AppImage",
+        if not os.path.exists("output/%s.stl" % name) or opts.rebuild:
+            print("Building output/%s.stl" % name)
+            os.system(" ".join([
+                opts.executable,
                 "-o",
-                f"output/{name}.stl",
+                "output/%s.stl" % name,
                 "-p",
                 "swatches.json",
                 "-P",
-                f"{name}",
+                name,
                 "Configurable_Filament_Swatch_VW.scad",
-            ])
+            ]))
